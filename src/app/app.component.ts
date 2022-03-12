61ef0e44e32707e624da9ef1119ff5517bdb4f27
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-03-11 20:49:02 -0500

    🔥 Chore: Able to add data to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  todoArray: string[] = [];
   title = 'ng-todo';
+
+  addTodo(value: string) {
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
 }
